3773f700cad737a3c34b4d4d17ccd91217b0d31c
gregorgasparac <gasparac.gregor8@gmail.com>
2023-04-05 17:54:19 +0000

    5april

--- database.py
+++ database.py
@@ -9,3 +9,8 @@ class Forum(Base):
     __tablename__ = "zapisi"
     id = Column(Integer, primary_key=True)
     text = Column(String(50))
+
+class Topics(Base):
+    __tablename__ = 'topics'
+    id = Column(Integer, primary_key=True)
+    topic = Column(String(50), nullable=False)
